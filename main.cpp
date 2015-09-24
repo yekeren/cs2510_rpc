20219de2f2549450b3ca9f740a01e5ee04542c6a
Keren Ye <yekeren.cn@gmail.com>
2015-09-24 00:10:54 -0400

    create folders for include and src files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include "add_stub.h"
+//#include "add_stub.h"
 
 int main(int argc, char *argv[]) {
-    printf("%d\n", add_stub(2, 3));
+    //printf("%d\n", add_stub(2, 3));
     return 0;
 }
