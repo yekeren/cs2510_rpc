2f6be929cd1a6304f786e1dcb1406485b8126bd6
Keren Ye <yekeren.cn@gmail.com>
2015-10-06 16:22:46 -0400

    Revert "wc_counter"
    This reverts commit 8bb0fa703f6a63fc83cd1e84872e8d5056d0b170.

--- main_cli.cpp
+++ main_cli.cpp
@@ -1,13 +1,7 @@
 #include "rpc_log.h"
 #include "add_stub_cli.h"
-#include "wc_stub_cli.h"
-
-int multiply(int m, int n, int k[m][n]) {
-    return 0;
-}
 
 int main(int argc, char *argv[]) {
-    printf("sum=%d\n", add(10, 4));
-    printf("len=%d\n", wc("hello, world"));
+    printf("%d\n", add(10, 4));
     return 0;
 }
