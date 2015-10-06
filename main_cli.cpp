8bb0fa703f6a63fc83cd1e84872e8d5056d0b170
Keren Ye <yekeren.cn@gmail.com>
2015-10-06 15:34:42 -0400

    wc_counter

--- main_cli.cpp
+++ main_cli.cpp
@@ -1,7 +1,13 @@
 #include "rpc_log.h"
 #include "add_stub_cli.h"
+#include "wc_stub_cli.h"
+
+int multiply(int m, int n, int k[m][n]) {
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
-    printf("%d\n", add(10, 4));
+    printf("sum=%d\n", add(10, 4));
+    printf("len=%d\n", wc("hello, world"));
     return 0;
 }
